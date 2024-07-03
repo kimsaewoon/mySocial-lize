d378d65d2f99866f361c5b0693f306eb92f4a527
sw-kim <kimsaewoon@gmail.com>
2024-07-04 08:31:08 +0900

    healthcheck삭제

--- src/main/java/com/example/project/_core/config/HealthCheck.java
+++ src/main/java/com/example/project/_core/config/HealthCheck.java
@@ -3,10 +3,10 @@ package com.example.project._core.config;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
-public class HealthCheck {
-    @GetMapping("/health")
-    public String healthCheck() {
-        return "ok";
-    }
-}
+//@RestController
+//public class HealthCheck {
+//    @GetMapping("/health")
+//    public String healthCheck() {
+//        return "ok";
+//    }
+//}
